2f43620856d3fde8d53f369a336f144b57d317cb
mayur2226 <mayureshsevekar22@gmail.com>
2020-08-07 18:10:05 +0530

    library project

---
+++ p/firebase.js
@@ -0,0 +1,17 @@
+var app_fireBasease={};
+(function(){
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCFp_MsOeNd2qr5520D9eI-QGrfGiaNlZA",
+    authDomain: "reggggg-53598.firebaseapp.com",
+    databaseURL: "https://reggggg-53598.firebaseio.com",
+    projectId: "reggggg-53598",
+    storageBucket: "reggggg-53598.appspot.com",
+    messagingSenderId: "602819991893",
+    appId: "1:602819991893:web:ec83444f222e6766bd77dc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+	app_fireBase=firebase;
+	//app_fireBase.prototpe.setData=function()
+	})()
